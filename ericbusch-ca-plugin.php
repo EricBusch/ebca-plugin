be025693e8b9257adfed5557404de50880bc4d6e
Eric Busch <ericbusch@gmail.com>
2023-07-26 21:50:20 -0400

    Update ericbusch-ca-plugin.php

--- ericbusch-ca-plugin.php
+++ ericbusch-ca-plugin.php
@@ -12,6 +12,48 @@
 /**
  * Enable ACF Options page.
  */
-if( function_exists('acf_add_options_page') ) {
+if ( function_exists( 'acf_add_options_page' ) ) {
 	acf_add_options_page();
+}
+
+/**
+ * Get background image for homepage.
+ *
+ * @param string $size
+ *
+ * @return string|null
+ */
+function busch_get_homepage_bg_image( string $size = 'full' ): ?string {
+
+	$ids = busch_get_homepage_background_image_ids();
+
+	if ( ! $ids ) {
+		return null;
+	}
+
+	shuffle( $ids );
+
+	return wp_get_attachment_image_src( $ids[0], $size )[0] ?? null;
+}
+
+/**
+ * Get array of background image IDs for homepage.
+ *
+ * @return array
+ */
+function busch_get_homepage_background_image_ids(): array {
+
+	$rows = get_field( 'background_images', absint( get_option( 'page_on_front' ) ) );
+
+	$ids = [];
+
+	foreach ( $rows as $key => $value ) {
+		if ( isset( $value['image']['ID'] ) ) {
+			$ids[] = absint( $value['image']['ID'] );
+		}
+	}
+
+	$ids = array_unique( array_filter( $ids ) );
+
+	return $ids;
 }
